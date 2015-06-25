f69ccb88d3d3b5df2e6b60bb8bd7164f11ad26f1
Christian Meisberger <meisberc@hochschule-trier.de>
2015-06-25 22:02:58 +0200

    project changes

---
+++ app/src/main/java/database/Tour.java
@@ -0,0 +1,44 @@
+package database;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Christian Meisberger on 25.06.2015.
+ */
+public class Tour {
+    private String name;
+    private String desc;
+    private int numberOfPoints;
+    private ArrayList<Point> points;
+
+    public Tour(String name, String desc, int numberOfPoints, ArrayList<Point> points) {
+        this.name = name;
+        this.desc = desc;
+        this.numberOfPoints = numberOfPoints;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getNumberOfPoints() {
+        return numberOfPoints;
+    }
+
+    public ArrayList<Point> getPoints() {
+        return points;
+    }
+}
