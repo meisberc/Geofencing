b04864908c115c49fe09b6501241a2b169c70df8
Christian Meisberger <meisberc@hochschule-trier.de>
2015-06-25 23:13:58 +0200

    adapter

--- app/src/main/java/database/Point.java
+++ app/src/main/java/database/Point.java
@@ -4,8 +4,11 @@ package database;
  * Created by Christian Meisberger on 25.06.2015.
  */
 public class Point {
+    private int id;
     private String name;
     private String desc;
+    private double latitude;
+    private double longitude;
 
     public Point(String name, String desc) {
         this.name = name;
