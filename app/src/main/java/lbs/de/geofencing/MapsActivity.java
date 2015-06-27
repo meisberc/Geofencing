6afbe12ecb5e6c82940a0eb49e2ee8b65a11b812
Christian Meisberger <meisberc@hochschule-trier.de>
2015-06-27 18:34:58 +0200

    Zeichnen der Strecke zum nächsten Punkt implementiert

--- app/src/main/java/lbs/de/geofencing/MapsActivity.java
+++ app/src/main/java/lbs/de/geofencing/MapsActivity.java
@@ -4,7 +4,9 @@ import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
@@ -24,9 +26,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import database.DbAdapter;
@@ -34,6 +40,8 @@ import database.Point;
 import geofence.Constants;
 import geofence.GeofenceTransitionsIntentService;
 import gpstracker.GPSTracker;
+import path.HttpConnection;
+import path.PathJSONParser;
 
 public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     protected static final String TAG = "monitoring-geofences";
@@ -78,7 +86,6 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
         setUpMapIfNeeded();
 
 
-
         setUpListener();
 
         // Get the geofences used.
@@ -96,12 +103,15 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
         }
 
         newPoint = points.get(0);
-        LatLng newLatLng = new LatLng(newPoint.getLatitude(),newPoint.getLongitude());
-        if(myLocation != null) {
-           // LatLng oldLatLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-            LatLng oldLatLng = new LatLng(points.get(1).getLatitude(), points.get(1).getLongitude());
-        }
+        LatLng newLatLng = new LatLng(newPoint.getLatitude(), newPoint.getLongitude());
+        LatLng oldLatLng = new LatLng(points.get(1).getLatitude(), points.get(1).getLongitude());
+        if (myLocation != null) {
+            // LatLng oldLatLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
 
+        }
+        String url = getDirectionsUrl(oldLatLng, newLatLng);
+        ReadTask downloadTask = new ReadTask();
+        downloadTask.execute(url);
 
     }
 
@@ -145,6 +155,32 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
         }
     }
 
+    private String getDirectionsUrl(LatLng origin, LatLng dest) {
+
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+
+        //Mode walking
+        String mode = "mode=walking";
+
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + mode + "&" + sensor;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+        return url;
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -155,11 +191,11 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
      * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
+     * <p>
      * If it isn't installed {@link SupportMapFragment} (and
      * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
      * install/update the Google Play services APK on their device.
-     * <p/>
+     * <p>
      * A user can return to this FragmentActivity after following the prompt and correctly
      * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
      * have been completely destroyed during this process (it is likely that it would only be
@@ -184,7 +220,7 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
-     * <p/>
+     * <p>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
@@ -348,6 +384,76 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
 
                             // Create the geofence.
                     .build());
+        }
+    }
+
+    private class ReadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... url) {
+            String data = "";
+            try {
+                HttpConnection http = new HttpConnection();
+                data = http.readUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            new ParserTask().execute(result);
+        }
+    }
+
+    private class ParserTask extends
+            AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(
+                String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                PathJSONParser parser = new PathJSONParser();
+                routes = parser.parse(jObject);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> routes) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions polyLineOptions = null;
+
+            // traversing through routes
+            for (int i = 0; i < routes.size(); i++) {
+                points = new ArrayList<LatLng>();
+                polyLineOptions = new PolylineOptions();
+                List<HashMap<String, String>> path = routes.get(i);
+
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                polyLineOptions.addAll(points);
+                polyLineOptions.width(2);
+                polyLineOptions.color(Color.BLUE);
+            }
+
+            mMap.addPolyline(polyLineOptions);
         }
     }
 }
